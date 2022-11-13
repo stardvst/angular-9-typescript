5a6020ddc0c53ae6247cf119513e1ebcb06cbd4c
stardvst <lilia.manukyan@hotmail.com>
2022-11-13 17:14:11 +0400

    using formbuilder

--- model-driven-forms/src/app/login.component.ts
+++ model-driven-forms/src/app/login.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { FormGroup, FormBuilder, /* FormControl, */ Validators, Form } from '@angular/forms';
 @Component({
   selector: 'login',
   templateUrl: 'login.component.html',
 })
 export class LoginComponent {
-  form = new FormGroup({
-    username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
-  });
+  // form = new FormGroup({
+  //   username: new FormControl('', Validators.required),
+  //   password: new FormControl('', Validators.required),
+  // });
+
+  form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   login() {
     console.log(this.form.value);
