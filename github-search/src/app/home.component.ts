719f84e25082e485e010b7277290a7abbe1edb39
stardvst <lilia.manukyan@hotmail.com>
2022-11-19 18:09:39 +0400

    imperative navigation

--- github-search/src/app/home.component.ts
+++ github-search/src/app/home.component.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  template: '<h1>Home</h1>',
+  template: `
+    <h1>Home</h1>
+    <button (click)="navigateToUsers()">Go to Github users</button>
+  `,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private _router: Router) {}
+
+  navigateToUsers() {
+    this._router.navigate(['github']);
+  }
+}
