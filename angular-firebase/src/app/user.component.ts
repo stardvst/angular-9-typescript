5b8ec5d7de2654c03d04e2aa691dfce840199888
stardvst <lilia.manukyan@hotmail.com>
2022-11-23 18:55:49 +0400

    added deleting a user from firestore db

--- angular-firebase/src/app/user.component.ts
+++ angular-firebase/src/app/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
+import { map } from 'rxjs';
 
 interface User {
   name: string;
@@ -19,10 +20,25 @@ export class UserComponent implements OnInit {
 
   ngOnInit(): void {
     this.usersCol = this.afs.collection('users');
-    this.users = this.afs.collection('users').valueChanges();
+    this.users = this.usersCol.snapshotChanges().pipe(
+      map(actions =>
+        actions.map(action => {
+          const data = action.payload.doc.data() as User;
+          const id = action.payload.doc.id;
+          console.log(id, data);
+          return { id, data };
+        })
+      )
+    );
   }
 
   addUser() {
     this._router.navigate(['add']);
+  }
+
+  deleteUser(userId: string, name: string) {
+    if (confirm(`Are you sure you want to delete ${name}?`)) {
+      this.afs.doc('users/' + userId).delete();
+    }
   }
 }
