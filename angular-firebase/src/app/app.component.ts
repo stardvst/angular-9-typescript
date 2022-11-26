f30e4b1d8d3fe445529bfbc3d6bd3b8e2b9addde
stardvst <lilia.manukyan@hotmail.com>
2022-11-26 18:26:31 +0400

    added login/logout header links

--- angular-firebase/src/app/app.component.ts
+++ angular-firebase/src/app/app.component.ts
@@ -1,14 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  template: `
+    <nav class="navbar navbar-expand-lg navbar-light ng-light">
+      <div id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" (click)="onLogout()" *ngIf="isLoggedIn | async" href="">
+              Logout<span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" routerLink="login" *ngIf="!(isLoggedIn | async)" href="">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" routerLink="signup" *ngIf="!(isLoggedIn | async)" href="">Sign up</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent implements OnInit {
+  isLoggedIn: Observable<boolean> | null = null;
+
   constructor(private _loginService: LoginService) {}
 
   ngOnInit() {
     this._loginService.getCurrentUser();
+    this.isLoggedIn = this._loginService.isLoggedIn;
+  }
+
+  onLogout() {
+    this._loginService.logout();
   }
 }
